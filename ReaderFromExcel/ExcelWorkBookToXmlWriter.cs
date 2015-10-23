e23a1492ca450b5429aa514531478d0d678e7e61
Vladimir Toth <vladimir.toth@datapac.sk>
2015-10-23 08:42:57 +0200

    XML

--- ReaderFromExcel/ExcelWorkBookToXmlWriter.cs
+++ ReaderFromExcel/ExcelWorkBookToXmlWriter.cs
@@ -8,16 +8,7 @@ using System.Xml.Serialization;
 
 namespace ReaderFromExcel
 {
-    class ExcelWorkBookToXmlWriter:IExcelWorkBookWriter
+    class ExcelWorkBookToXmlWriter//:IExcelWorkBookWriter
     {
-        public void WriteExcelWorkBook(ExcelWorkbook workBook, bool rewrite)
-        {
-            XmlSerializer x = new XmlSerializer(typeof(ExcelWorkbook));
-            using (FileStream file = new FileStream("cesta.xml", FileMode.OpenOrCreate))
-            using (TextWriter tw = new StreamWriter(file))
-            {
-                x.Serialize(tw, workBook);
-            }
-        }
     }
 }
